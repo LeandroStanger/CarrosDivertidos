6d3f3a6b846871d56d0cfa828c8b29bf893ffbe4
Leandro Stanger <leandrostanger@hotmail.com>
2025-11-09 11:53:28 -0300

    v0.2.5

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ function parar() {
     clearInterval(movimento);
     movimento = null;
 }
+
+document.getElementById('contato').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    fetch(this.action, {
+        method: this.method,
+        body: new FormData(this),
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        window.location.href = 'https://leandrostanger.github.io/CarrosDivertidos/obrigado.html';
+    }).catch(error => {
+        console.error('Erro:', error);
+    });
+});
